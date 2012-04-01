2716a606d073dff1b2fafa8d94d5ab267d5d960f
Will Milton <wa.milton@gmail.com>
2012-04-01 16:26:22 -0500

    actually committing the files in question....

---
+++ lib/AcceptLanguage.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * This file is part of BadFaith.
+ *
+ * Copyright (c) 2012 William Milton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace BadFaith;
+/**
+ * BadFaith container for Accept-* parsing
+ *
+ * @package BadFaith
+ * @author William Milton
+ */
+class AcceptLanguage extends AcceptLike {
+
+  public $pref;
+  public $params;
+  public $q;
+  public $type;
+  public $subtype;
+
+  function __construct($header_str = NULL) {
+    parent::__construct($header_str);
+    $this->init();
+  }
+
+  static function __set_state(array $properties) {
+    parent::__set_state($properties);
+    $accept = new AcceptLanguage();
+    foreach ($properties as $key=>$prop) {
+      if (!property_exists('AcceptLike', $key)) {
+        if (property_exists($accept, $key)) {
+          $accept->$key = $prop;
+        }
+      }
+    }
+    return $accept_like;
+  }
+
+  function init() {
+    $parts = explode('-', $this->pref, 2);
+    if (!array_key_exists(1, $parts)) {
+      $parts[1] = NULL;
+    }
+    $this->lang = $parts[0];
+    $this->sublang = $parts[1];
+  }
+}
---
+++ tests/BadFaith/Tests/AcceptLanguageTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of BadFaith.
+ *
+ * Copyright (c) 2012 William Milton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Badfaith tests for Accept-* parsing
+ *
+ * @package BadFaith
+ * @author William Milton
+ */
+
+namespace BadFaith\Tests;
+
+use AcceptLanguage;
+
+class AcceptLanguageTest extends \PHPUnit_Framework_TestCase {
+
+  public function setUp() {
+    $this->headers = array (
+      'accept' => 'text/html;level=2;q=0.7,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'accept_encoding' => 'gzip,deflate,sdch',
+      'accept_language' => 'en-US,en;q=0.8',
+      'accept_charset' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    );
+    $this->accept_language_split = array(
+      'en-US',
+      'en;q=0.8',
+    );
+  }
+
+  public function testInitWithString() {
+    $accept = new \BadFaith\AcceptLanguage($this->accept_language_split[0]);
+    $expected = new \BadFaith\AcceptLanguage();
+    $expected->pref = 'en-US';
+    $expected->params = array();
+    $expected->q = 1.0;
+    $expected->lang = 'en';
+    $expected->sublang = 'US';
+    $this->assertEquals($expected, $accept);
+    $accept = new \BadFaith\AcceptLanguage($this->accept_language_split[1]);
+    $expected->pref = 'en';
+    $expected->lang = 'en';
+    $expected->sublang = NULL;
+    $expected->q = 0.8;
+    $this->assertEquals($expected, $accept);
+  }
+}
